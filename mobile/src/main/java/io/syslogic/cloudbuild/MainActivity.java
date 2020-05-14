db00bbdfc480f0fa69327321e95391cfd12d847a
Martin Zeitler <syslogic23@gmail.com>
2020-05-14 22:54:50 +0200

    Android example navigation fixed.

--- mobile/src/main/java/io/syslogic/cloudbuild/MainActivity.java
+++ mobile/src/main/java/io/syslogic/cloudbuild/MainActivity.java
@@ -1,7 +1,15 @@
 package io.syslogic.cloudbuild;
 
-import android.app.Activity;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+    }
 }
---
+++ mobile/src/main/java/io/syslogic/cloudbuild/MainFragment.java
@@ -0,0 +1,16 @@
+package io.syslogic.cloudbuild;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, parent, false);
+    }
+}
