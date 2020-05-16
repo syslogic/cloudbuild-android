f46d49fcf84cefe3eded23347d82280b878f88a4
Martin Zeitler <martin@syslogic.io>
2020-05-16 07:45:17 +0200

    scripts updated.

--- scripts/kms_encrypt.sh
+++ scripts/kms_encrypt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gcloud kms encrypt --plaintext-file=../keystore.properties --ciphertext-file=../credentials/keystore.properties.enc --location=global --keyring=android-gradle --key=default
+gcloud kms encrypt --plaintext-file=../credentials/google-service-account.json --ciphertext-file=../credentials/google-service-account.json --location=global --keyring=android-gradle --key=default
 gcloud kms encrypt --plaintext-file=../mobile/google-services.json --ciphertext-file=../credentials/google-services.json.enc --location=global --keyring=android-gradle --key=default
 gcloud kms encrypt --plaintext-file=~/.android/debug.keystore --ciphertext-file=../credentials/debug.keystore.enc --location=global --keyring=android-gradle --key=default
 gcloud kms encrypt --plaintext-file=~/.android/release.keystore --ciphertext-file=../credentials/release.keystore.enc --location=global --keyring=android-gradle --key=default
