cc3a304e7e586d6405050dccadf90b60453b47e2
Martin Zeitler <martin@syslogic.io>
2020-05-17 08:49:45 +0200

    scripts updated.

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -10,7 +10,7 @@ rm /workspace/cloudbuild.yaml
 rm /workspace/Dockerfile
 rm /workspace/README.md
 rm /workspace/LICENSE
-
+# ls -la
 
 # install Android command-line tools (has sdkmanager)
 # https://developer.android.com/studio#command-tools
@@ -28,29 +28,35 @@ rm ${CLI_TOOLS_ZIPFILE}
 yes | ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --licenses >/dev/null
 #${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --list
 
-# always install Android platform tools
+# always install Android Platform Tools
 ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "platform-tools"
+PACKAGES="platform-tools"
 
 # install Android SDK Platform
 if [ "x$ANDROID_SDK_PLATFORM" = "x" ] ; then
     echo _ANDROID_SDK_PLATFORM not provided, skipping install. ;
 else
-    ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "platforms;android-${ANDROID_SDK_PLATFORM}"
+    # ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "platforms;android-${ANDROID_SDK_PLATFORM}"
+    ${PACKAGES}+=" platforms;android-${ANDROID_SDK_PLATFORM}"
 fi
 
 # install Android SDK Build-Tools
 if [ "x$BUILD_TOOLS_VERSION" = "x" ] ; then
     echo _BUILD_TOOLS_VERSION not provided, skipping install. ;
 else
-    ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "build-tools;${BUILD_TOOLS_VERSION}"
+    #${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "build-tools;${BUILD_TOOLS_VERSION}"
+    ${PACKAGES}+=" build-tools;${BUILD_TOOLS_VERSION}"
 fi
 
 # install Android NDK
 if [ "x$ANDROID_NDK_VERSION" = "x" ] ; then
     echo _ANDROID_NDK_VERSION not provided, skipping install. ;
 else
-    ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "ndk;${ANDROID_NDK_VERSION}"
+    #${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "ndk;${ANDROID_NDK_VERSION}"
+    ${PACKAGES}+=" ndk;${ANDROID_NDK_VERSION}"
 fi
+
+${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} ${PACKAGES}
 
 # change Gradle wrapper version; eg. from version 5.6.4 to 6.4.1
 if [ "x$GRADLE_VERSION" = "x" ] ; then
@@ -61,5 +67,3 @@ else
         sed -i -e "s/5\.6\.4/${GRADLE_VERSION}/g" ${WRAPPER_PROPERTIES}
     fi
 fi
-
-# ls -la
