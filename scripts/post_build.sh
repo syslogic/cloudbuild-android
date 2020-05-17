358c212d380822a329ad442ec311345d5bc7ebc8
Martin Zeitler <martin@syslogic.io>
2020-05-17 06:56:21 +0200

    scripts updated.

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -2,5 +2,7 @@
 # post-build; written 2020 by Martin Zeitler
 
 shopt -s dotglob
-rm -r /root/.gradle/caches/*
 rm -r /workspace/*
+if [ -d "/root/.gradle/caches" ]; then
+    rm -r /root/.gradle/caches/*
+fi
