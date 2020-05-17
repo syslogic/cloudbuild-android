8fd3d32f61478f6c262a99a59339fd54b3279a31
Martin Zeitler <martin@syslogic.io>
2020-05-17 09:00:46 +0200

    scripts updated.

--- scripts/pre_build.sh
+++ scripts/pre_build.sh
@@ -16,8 +16,7 @@ rm /workspace/LICENSE
 # https://developer.android.com/studio#command-tools
 # https://developer.android.com/studio/command-line/sdkmanager.html
 if [ "x$ANDROID_SDK_VERSION" = "x" ] ; then
-    _ANDROID_SDK_VERSION not provided, using the default version.
-    ANDROID_SDK_VERSION = 6200805 ;
+    ANDROID_SDK_VERSION=6200805 ;
 fi
 CLI_TOOLS_ZIPFILE=commandlinetools-linux-${ANDROID_SDK_VERSION}_latest.zip
 wget -q https://dl.google.com/android/repository/${CLI_TOOLS_ZIPFILE}
@@ -37,7 +36,7 @@ if [ "x$ANDROID_SDK_PLATFORM" = "x" ] ; then
     echo _ANDROID_SDK_PLATFORM not provided, skipping install. ;
 else
     # ${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "platforms;android-${ANDROID_SDK_PLATFORM}"
-    ${PACKAGES}+=" platforms;android-${ANDROID_SDK_PLATFORM}"
+    $PACKAGES="${PACKAGES} platforms;android-${ANDROID_SDK_PLATFORM}"
 fi
 
 # install Android SDK Build-Tools
@@ -45,7 +44,7 @@ if [ "x$BUILD_TOOLS_VERSION" = "x" ] ; then
     echo _BUILD_TOOLS_VERSION not provided, skipping install. ;
 else
     #${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "build-tools;${BUILD_TOOLS_VERSION}"
-    ${PACKAGES}+=" build-tools;${BUILD_TOOLS_VERSION}"
+    $PACKAGES="${PACKAGES} build-tools;${BUILD_TOOLS_VERSION}"
 fi
 
 # install Android NDK
@@ -53,10 +52,10 @@ if [ "x$ANDROID_NDK_VERSION" = "x" ] ; then
     echo _ANDROID_NDK_VERSION not provided, skipping install. ;
 else
     #${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} "ndk;${ANDROID_NDK_VERSION}"
-    ${PACKAGES}+=" ndk;${ANDROID_NDK_VERSION}"
+    $PACKAGES="${PACKAGES} ndk;${ANDROID_NDK_VERSION}"
 fi
 
-${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --install ${PACKAGES}
+${ANDROID_HOME}/tools/bin/sdkmanager --sdk_root=${ANDROID_HOME} --install $PACKAGES
 
 # change Gradle wrapper version; eg. from version 5.6.4 to 6.4.1
 if [ "x$GRADLE_VERSION" = "x" ] ; then
