5c33628cbecccdefcd6f20071425ae45bcb3a1dc
Martin Zeitler <martin@syslogic.io>
2020-05-17 23:18:40 +0200

    scripts updated.

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # post-build; written 2020 by Martin Zeitler
 
+# Removing the Android application from the container
 shopt -s dotglob
 rm -r /workspace/*
 
-# This directory only exists when having built
+# This directory only exists when having built with ./gradlew build
 if [ -d /root/.gradle/caches ]; then
     rm -r /root/.gradle/caches/*
 fi
