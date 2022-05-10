9da7239fe7317b706b26c9d87a0c9f33066ba274
Martin Zeitler <martin@syslogic.io>
2022-05-10 10:41:42 +0200

    example app dependencies updated.

--- mobile/src/main/java/io/syslogic/cloudbuild/MainFragment.java
+++ mobile/src/main/java/io/syslogic/cloudbuild/MainFragment.java
@@ -5,12 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class MainFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, parent, false);
     }
 }
